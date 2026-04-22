ddf8959c3ebe8bd8ffdfd506b1ac5c9f9c4a3e11
lujiang <lujiang0111@gmail.com>
2026-04-22 14:32:16 +0800

    更新运行脚本

--- conf/debug.sh
+++ conf/debug.sh
@@ -1,32 +1,36 @@
-#!/bin/bash
-shell_path=$(
+#!/usr/bin/env bash
+if [ -z "${BASH_VERSION:-}" ]; then
+    exec bash "$0" "$@"
+fi
+
+shell_dir=$(
     cd "$(dirname "$0")" || exit
     pwd
 )
-lib_path=${shell_path}/lib
+shell_dir=$(realpath "${shell_dir}")
+
 project=pcap_recorder2
+lib_dir=${shell_dir}/lib
 
 ulimit -n 65536
-export LD_LIBRARY_PATH=${lib_path}:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${lib_dir}:${LD_LIBRARY_PATH}
 
-cd "${lib_path}" || exit
-for file in *.so.*; do
-    if [ -f "${file}" ]; then
-        realname=$(echo "${file}" | rev | cut -d '/' -f 1 | rev)
-        libname=$(echo "${realname}" | cut -d '.' -f 1)
-        if [ ! -f "${libname}".so ]; then
-            ln -sf "${realname}" "${libname}".so
-        fi
-    fi
+cd "${lib_dir}" || exit
+for file in ./*.so.*; do
+    [ -f "${file}" ] || continue
+
+    realname=${file#./}
+    libname=${realname%%.so.*}.so
+    [ -e "${libname}" ] || ln -sf "${realname}" "${libname}"
 done
 ldconfig -n .
 
+cd "${shell_dir}" || exit
 runlog_max_size=10000000
-cd "${shell_path}" || exit
 if [ -f runlog ]; then
     runlog_size=$(stat --format=%s runlog)
     if [ "${runlog_size}" -gt ${runlog_max_size} ]; then
-        echo -e "runlog too big, restart at $(date)" >runlog
+        echo -e "runlog too big, restart at $(date +"%Y-%m-%d %H:%M:%S")" >runlog
     fi
 fi
 
@@ -35,10 +39,10 @@ function TrapSigint() {
 }
 trap TrapSigint 2
 
-echo -e "${project}-debug start at $(date)" >>runlog
+echo -e "${project}-debug start at $(date +"%Y-%m-%d %H:%M:%S")" >>runlog
 
-cd "${shell_path}" || exit
+cd "${shell_dir}" || exit
 chmod +x ${project}
 gdb --args ${project} "$@"
 
-echo -e "${project}-debug stop at $(date)" >>runlog
+echo -e "${project}-debug stop at $(date +"%Y-%m-%d %H:%M:%S")" >>runlog
